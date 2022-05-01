d7fca17481f88be8377d3c9dd70c9299e3c40481
fongziyjun16 <fongziyjun16@hotmail.com>
2022-05-01 17:30:56 -0400

    udpate

---
+++ TN/app/src/main/java/com/cyj/tinnews/model/Article.java
@@ -0,0 +1,15 @@
+package com.cyj.tinnews.model;
+
+public class Article {
+
+    public String author;
+    public String content;
+    public String description;
+    public String publishedAt;
+    public String title;
+    public String url;
+    public String urlToImage;
+
+
+
+}
---
+++ TN/app/src/main/java/com/cyj/tinnews/model/NewsResponse.java
@@ -0,0 +1,13 @@
+package com.cyj.tinnews.model;
+
+import java.util.List;
+
+public class NewsResponse {
+
+    public String status;
+    public Integer totalResults;
+    public List<Article> articles;
+    public String code;
+    public String message;
+
+}
