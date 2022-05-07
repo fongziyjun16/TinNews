2e307229916e5b0f40640856ac52174c8c7be3e6
fongziyjun16 <fongziyjun16@hotmail.com>
2022-05-07 12:01:47 -0400

    update

--- TN/app/src/main/java/com/cyj/tinnews/ui/home/HomeFragment.java
+++ TN/app/src/main/java/com/cyj/tinnews/ui/home/HomeFragment.java
@@ -1,7 +1,6 @@
 package com.cyj.tinnews.ui.home;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +11,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.cyj.tinnews.databinding.FragmentHomeBinding;
+import com.cyj.tinnews.model.Article;
 import com.cyj.tinnews.repository.NewsRepository;
 import com.cyj.tinnews.repository.NewsViewModelFactory;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.StackFrom;
+
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel viewModel;
     private FragmentHomeBinding binding;
+    private CardStackLayoutManager layoutManager;
+    private List<Article> articles;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,13 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        CardSwipeAdapter swipeAdapter = new CardSwipeAdapter();
+        layoutManager = new CardStackLayoutManager(requireContext());
+        layoutManager.setStackFrom(StackFrom.Top);
+        binding.homeCardStackView.setLayoutManager(layoutManager);
+        binding.homeCardStackView.setAdapter(swipeAdapter);
+
+
         NewsRepository repository = new NewsRepository();
         // viewModel = new HomeViewModel(repository);
         viewModel = new ViewModelProvider(this,
@@ -49,7 +62,9 @@ public class HomeFragment extends Fragment {
                         getViewLifecycleOwner(),
                         newsResponse -> {
                             if (newsResponse != null) {
-                                Log.d("HomeFragment", newsResponse.toString());
+//                                Log.d("HomeFragment", newsResponse.toString());
+                                articles = newsResponse.articles;
+                                swipeAdapter.setArticles(articles);
                             }
                         });
     }
